0d1a181b6bbae381423f9ecde0e761e0184122cc
RAUSHAN-SONALI-143 <156349324+RAUSHAN-SONALI-143@users.noreply.github.com>
2025-01-24 14:02:17 +0530

    ᴛᴇᴀᴍ ᴘᴜʀᴠɪ ʙᴏᴛs

--- config.py
+++ config.py
@@ -138,22 +138,18 @@ confirmer = {}
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
-START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/yeeu8p.jpg"
-)
-PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/yeeu8p.jpg"
-)
-PLAYLIST_IMG_URL = "https://telegra.ph/file/08db892ae10fcd27fc420.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/1827dd6c414ce09098002.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/08db892ae10fcd27fc420.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/08db892ae10fcd27fc420.jpg"
-STREAM_IMG_URL = "https://telegra.ph/file/03efec694e41e891b29dc.jpg"
-SOUNCLOUD_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
-YOUTUBE_IMG_URL = "https://telegra.ph/file/4dc854f961cd3ce46899b.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/6c741a6bc1e1663ac96fc.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/6c741a6bc1e1663ac96fc.jpg"
+START_IMG_URL = getenv("START_IMG_URL", "https://files.catbox.moe/fu6jk3.jpg")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://files.catbox.moe/26nzoq.jpg")
+PLAYLIST_IMG_URL = "https://files.catbox.moe/f2s4ws.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/z0gh23.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/2y5o3g.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/2y5o3g.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/2y5o3g.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/2y5o3g.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/2y5o3g.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg"
 
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
