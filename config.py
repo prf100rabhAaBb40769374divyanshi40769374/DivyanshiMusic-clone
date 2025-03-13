7c976dbc5a31e03f3537cf2941abc8d817761648
𝖲ɪɢᴍᴀ 𝖢ᴏᴅᴇʀ <156349324+The-SigmaCoder@users.noreply.github.com>
2025-03-13 10:08:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/RAUSHAN-SONALI-143/PURVIV2",
+    "https://github.com/The-SigmaCoder/PURVIV2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
